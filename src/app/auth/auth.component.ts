ebb6605893438451100baf99e16276ec2b9c12e5
ZouAlan <alanchromecast2014@gmail.com>
2017-03-15 23:45:23 +0800

    edit comment

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -14,21 +14,20 @@ export class AuthComponent implements AfterViewChecked {
 	constructor() { }
 
 	ngAfterViewChecked() {
-		//Alan:如果是載入或是點擊route事件的時候
+		//Alan:when the event is load or click(change route)
 		if (event && (event.type == "load" || event.type == "click")) {
 			this.setSize(window.innerHeight);
 		}
 	}
-
+z
 	@HostListener('window:resize', ['$event']) onResize(event) {
-		//Alan:window height decrease tag height
 		this.setSize(event.target.innerHeight);
 	}
 
 	private setSize(height: number) {
+		//Alan:window height decrease tag height
 		let marginHeight = (height - this.formTag.nativeElement.offsetHeight) * 0.5;
 
 		this.formTag.nativeElement.style.margin = `${marginHeight}px auto`;
-
 	}
 }
